96e58afe17b35fbb4361157303cb6e7d3d35d3ff
Zulleyy3 Zulleyy3 <20947639+Zulleyy3@users.noreply.github.com>
2020-01-14 17:57:21 +0100

    updated to homebrew redirects

--- ColonelBot-v4/Tools/EmbedTool.cs
+++ ColonelBot-v4/Tools/EmbedTool.cs
@@ -156,7 +156,7 @@ namespace ColonelBot_v4.Tools
             embed.AddField("How To Netbattle Online [Video Guides]", "These will show you how to import your save file, netbattle as a host, and netbattle as a Client.  \nhttp://bit.ly/2htNN8W");
             embed.AddField("Netbattle 101", "Overview of version differences and game mechanics for new and returning Netbattlers that need a refresher. Includes text guide of how to setup online play. \nhttps://docs.google.com/document/d/1AjvxhqfcMCg6H6o8683emQ7pmUq2ffGo0ogBPd84e00/");
 	    embed.AddField("Netbattle EX/Advanced Guides", "In-depth collection of guides with chip functionality, frame data, detailed game mechanic explanations as well as Japanese version exclusive e-reader+ information. \nhttps://docs.google.com/document/d/1Io4EU7S5lYtZS7bmgnfLBOl97umbMRodZEbXgFxTzdI");
-            embed.AddField("N1GP Central Document", "Access everything above and more through the central doc, a complete collection of all guides and resources provided by our community. \nhttps://tinyurl.com/centraldoc");
+            embed.AddField("N1GP Central Document", "Access everything above and more through the central doc, a complete collection of all guides and resources provided by our community. \nhttp://central.n1gp.net/");
 	    return embed.Build();
         }
 
